1795c12bb81bd6b958d9a50fbe6d5912958f395d
Yu357 <forestgreen14689@gmail.com>
2023-10-04 17:13:15 +0900

    feat: ページタイトルを設定

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
+import Head from "next/head";
+
 export default function Home() {
-  return (
-    <main>
-			<h1>Hello World</h1>
-		</main>
-  )
+	return (
+
+		<>
+			<Head>
+				<title>Skyline</title>
+			</Head>
+
+			<main>
+				<h1>Skyline</h1>
+			</main>
+		</>
+	)
 }
