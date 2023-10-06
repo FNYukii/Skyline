1ee339868963cc372a451ac883e1168e1338010b
FNYukii <forestgreen14689@gmail.com>
2023-10-07 01:06:25 +0900

    feat: 検索バーにアイコン設置

--- src/components/sections/SearchSection.tsx
+++ src/components/sections/SearchSection.tsx
@@ -1,13 +1,43 @@
+import { useState } from "react"
+import { AiOutlineClose, AiOutlineSearch } from "react-icons/ai"
+
 interface Props {
 	className?: string
 }
 
 function SearchSection(props: Props) {
 
+	const [keyword, setKeyword] = useState("")
+
 	return (
+
 		<div className={props.className}>
 
-			<input type="text" placeholder="検索" className="px-4 py-2 w-full bg-gray-100 placeholder:text-gray-400 rounded-none outline-blue-500" />
+			<div className="relative">
+
+				{keyword === "" &&
+					<div className="absolute pointer-events-non">
+
+						<div className="mt-2 ml-4 flex items-center gap-2">
+
+							<AiOutlineSearch className="text-gray-400" />
+							<span className="text-gray-400">検索</span>
+						</div>
+					</div>
+				}
+
+				{keyword !== "" &&
+					<div className="absolute w-full flex justify-end">
+
+						<button onClick={() => setKeyword("")} className="mt-1 mr-2 p-2 rounded-full hover:bg-gray-200 transition">
+							<AiOutlineClose className="text-gray-400" />
+						</button>
+					</div>
+				}
+
+				<input type="text" value={keyword} onChange={(event) => setKeyword(event.target.value)} className="px-4 py-2 w-full bg-gray-100 rounded-none outline-blue-500" />
+			</div>
+
 		</div>
 	)
 }
