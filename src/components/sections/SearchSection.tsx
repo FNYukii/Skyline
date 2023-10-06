140c473fd844ff77040ad69202d9b8164c3c0914
FNYukii <forestgreen14689@gmail.com>
2023-10-07 00:51:26 +0900

    feat: inputのoutline設定

--- src/components/sections/SearchSection.tsx
+++ src/components/sections/SearchSection.tsx
@@ -7,7 +7,7 @@ function SearchSection(props: Props) {
 	return (
 		<div className={props.className}>
 
-			<input type="text" placeholder="検索" className="px-4 py-2 w-full bg-gray-100 placeholder:text-gray-400 rounded-none" />
+			<input type="text" placeholder="検索" className="px-4 py-2 w-full bg-gray-100 placeholder:text-gray-400 rounded-none outline-blue-500" />
 		</div>
 	)
 }
