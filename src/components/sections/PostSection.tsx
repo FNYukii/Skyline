008b02b1169fb7e7043f812bebd8f14ba1fd8443
FNYukii <forestgreen14689@gmail.com>
2023-10-08 16:49:38 +0900

    feat: サムネイル画像を表示

--- src/components/sections/PostSection.tsx
+++ src/components/sections/PostSection.tsx
@@ -12,6 +12,8 @@ function PostSection(props: Props) {
 		<div className={props.className}>
 
 			<h1 className="text-2xl font-bold">{props.post.title}</h1>
+			<img src={props.post.thumbnail} alt={props.post.title} className="mt-4 aspect-video object-cover" />
+			
 			<p>Article here</p>
 		</div>
 	)
