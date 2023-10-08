06ec21bc12766b634f2f312987030fc0d8489342
FNYukii <forestgreen14689@gmail.com>
2023-10-08 17:38:56 +0900

    clean: Section系コンポーネントの一番外側の要素は、className適用専用に統一

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -8,42 +8,45 @@ function Footer(props: Props) {
 
 	return (
 
-		<footer className={`bg-accent-color text-white pt-4 pb-4 ${props.className}`}>
+		<footer className={props.className}>
 
-			<div className="w-full lg:width-lg px-4 lg:px-0 mx-auto">
+			<div className="bg-accent-color text-white pt-4 pb-4">
 
-				<div className="flex">
+				<div className="w-full lg:width-lg px-4 lg:px-0 mx-auto">
 
-					<div className="w-1/3">
+					<div className="flex">
 
-						<Link href="/" className="text-4xl font-light">Skyline</Link>
-						<p className="mt-1 text-gray-400">好きなビルを語るブログ</p>
-					</div>
+						<div className="w-1/3">
 
-					<div className="w-1/3">
+							<Link href="/" className="text-4xl font-light">Skyline</Link>
+							<p className="mt-1 text-gray-400">好きなビルを語るブログ</p>
+						</div>
 
-						<p className="font-bold text-xl">Pages</p>
+						<div className="w-1/3">
 
-						<div className="mt-2 flex flex-col">
+							<p className="font-bold text-xl">Pages</p>
 
-							<Link href="/" className="text-gray-400 hover:underline">Top</Link>
-							<Link href="/map" className="text-gray-400 hover:underline">Map</Link>
-							<Link href="/gallery" className="text-gray-400 hover:underline">Gallery</Link>
+							<div className="mt-2 flex flex-col">
+
+								<Link href="/" className="text-gray-400 hover:underline">Top</Link>
+								<Link href="/map" className="text-gray-400 hover:underline">Map</Link>
+								<Link href="/gallery" className="text-gray-400 hover:underline">Gallery</Link>
+							</div>
+						</div>
+
+						<div className="w-1/3">
+
+							<p className="font-bold text-xl">Links</p>
+
+							<div className="mt-2 flex flex-col">
+
+								<a target="blank" href="https://github.com/FNYukii/Skyline" className="text-gray-400 hover:underline">Source</a>
+							</div>
 						</div>
 					</div>
 
-					<div className="w-1/3">
-
-						<p className="font-bold text-xl">Links</p>
-
-						<div className="mt-2 flex flex-col">
-
-							<a target="blank" href="https://github.com/FNYukii/Skyline" className="text-gray-400 hover:underline">Source</a>
-						</div>
-					</div>
+					<p className="mt-16 text-gray-400 text-center">Copyright Yu357 2023</p>
 				</div>
-
-				<p className="mt-16 text-gray-400 text-center">Copyright Yu357 2023</p>
 			</div>
 		</footer>
 	)
