857aa20fdfcad4e9758f46a7155cd9de26b1c6c4
FNYukii <forestgreen14689@gmail.com>
2023-10-09 19:52:14 +0900

    feat: モーダルに関するエラーを回避した

--- src/components/sections/PostSection.tsx
+++ src/components/sections/PostSection.tsx
@@ -3,7 +3,7 @@ import dayjs from "dayjs"
 import { AiOutlineEdit } from "react-icons/ai"
 import ReactMarkdown from "react-markdown"
 import remarkGfm from "remark-gfm"
-import ImageModalButton from "../others/ImageModalButton"
+import { useEffect, useState } from "react"
 
 interface Props {
 	className?: string
@@ -19,11 +19,29 @@ function PostSection(props: Props) {
 		)
 	}
 
+	// モーダルを開くbuttonタグに入ったimgタグ
 	function ImgExpandable({ ...props }) {
 		return (
-			<ImageModalButton src={props.src} alt={props.alt} className="mt-4" />
+			<button onClick={() => setOpenImage(props.src)} className="mt-4 hover:brightness-90 transition">
+				<img src={props.src} alt={props.alt} />
+			</button>
 		)
 	}
+
+	// 画像モーダル用フラグ
+	const [openImage, setOpenImage] = useState("")
+
+	// 画像モーダル展開中はスクロール禁止
+	useEffect(() => {
+
+		if (openImage !== "") {
+			document.body.style.overflow = "hidden"
+		}
+
+		if (openImage === "") {
+			document.body.style.overflow = "auto"
+		}
+	}, [openImage])
 
 	return (
 
@@ -43,7 +61,9 @@ function PostSection(props: Props) {
 				</div>
 			</div>
 
-			<ImageModalButton className="mt-4" src={props.post.thumbnail} alt={props.post.title} aspectVideo />
+			<button onClick={() => setOpenImage(props.post.thumbnail)} className="mt-4 hover:brightness-90 transition">
+				<img src={props.post.thumbnail} alt={props.post.title} className="aspect-video object-cover" />
+			</button>
 
 			<ReactMarkdown
 				children={props.post.content}
@@ -51,6 +71,15 @@ function PostSection(props: Props) {
 				components={{ h2: h2WithId, img: ImgExpandable }} // 特定のタグを自作の要素に置き換えて表示
 				className="post-content"
 			/>
+
+			{openImage !== "" &&
+				<div className="z-30 fixed top-0 left-0 w-full h-full flex justify-center items-center">
+
+					<div onClick={() => setOpenImage("")} className="w-full h-full bg-black/50"></div>
+
+					<img src={openImage} alt={"---"} className="absolute max-height-screen-95" />
+				</div>
+			}
 		</div>
 	)
 }
