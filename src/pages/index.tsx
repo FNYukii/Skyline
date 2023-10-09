5d537a00af5b200e736b0f6c9b0b605a6d51d685
FNYukii <forestgreen14689@gmail.com>
2023-10-09 16:50:03 +0900

    clean: 不要なコードを削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -85,7 +85,6 @@ function Home(props: Props) {
 							<SearchSection />
 							<TagListSection label="最近のタグ" className="mt-4" tags={TagService.recentlyTags(props.posts)} />
 							<TagListSection label="全てのタグ" className="mt-4" tags={TagService.allTags(props.posts)} />
-							{/* <TagListSection label="全てのタグ" className="mt-4" tags={["大阪市", "梅田", "住友不動産", "シティタワー", "ガーデン", "UR都市機構", "阪急阪神不動産", "ジオ", "ジオグランデ", "ジオタワー", "京都", "JR西日本", "ホテルグランヴィア", "JR西日本ホテルズ", "ブリリア", "東京建物", "森ビル", "東京", "新宿", "渋谷", "六本木", "虎ノ門", "赤坂", "病院", "ホテル", "外資系ホテル", "ヒルトン", "ウォルドーフ・アストリア", "コンラッド", "ダブルツリーbyヒルトン", "谷町", "天満橋", "大阪市北区", "大阪市中央区", "東京都港区", "大阪市淀川区", "阪急電鉄", "うめきた", "阪神電車", "近畿日本鉄道", "百貨店", "オフィスビル", "鉄筋鉄骨コンクリート造"]} /> */}
 						</div>
 					</div>
 				</div>
