3fa6af7868c521003f50e595eba6aab4a0d8c7a6
FNYukii <forestgreen14689@gmail.com>
2023-10-09 15:56:17 +0900

    feat: 目次の各項目に項番を付けた

--- src/components/sections/TableOfContentsSection.tsx
+++ src/components/sections/TableOfContentsSection.tsx
@@ -11,7 +11,10 @@ function TableOfContentsSection(props: Props) {
 	// id付き要素へ遷移するaタグ
 	const anckerToId = ({ ...props }) => {
 		return (
-			<a href={`#${props.children}`}>1. {props.children}</a>
+			<li>
+				<a href={`#${props.children}`}>{props.children}</a>
+
+			</li>
 		)
 	}
 
@@ -23,12 +26,15 @@ function TableOfContentsSection(props: Props) {
 
 				<p>目次</p>
 
-				<ReactMarkdown
-					children={props.content}
-					allowedElements={['h2']} // h2要素のみを表示する
-					components={{ h2: anckerToId }} // h2要素をanckerToIdに置き換えて表示
-					className="post-table-of-contents mt-2 flex flex-col gap-y-1"
-				/>
+				<ol className="list-decimal list-inside text-gray-600">
+
+					<ReactMarkdown
+						children={props.content}
+						allowedElements={['h2']} // h2要素のみを表示する
+						components={{ h2: anckerToId }} // h2要素をanckerToIdに置き換えて表示
+						className="post-table-of-contents mt-2 flex flex-col gap-y-1"
+					/>
+				</ol>
 			</div>
 		</div>
 	)
