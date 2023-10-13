24172a47f98a7bcc190708d0b9a441f62002fb06
FNYukii <forestgreen14689@gmail.com>
2023-10-13 10:12:25 +0900

    clean: コードを簡略化

--- src/components/sections/MapSection.tsx
+++ src/components/sections/MapSection.tsx
@@ -7,22 +7,16 @@ interface Props {
 
 function MapSection(props: Props) {
 
-	// 動的インポート
-	const Map = React.useMemo(() =>
-
-		dynamic(() => import("../../components/others/Map"), {
-			loading: () => <p>Loading...</p>,
-			ssr: false,
-		}),
-		[]
-	)
+	const DynamicMap = dynamic(() => import('../others/Map'), {
+		ssr: false,
+	})
 
 	return (
 		<div className={props.className}>
 
 			<h2 className='text-xl font-bold'>マップ</h2>
 			<div className='mt-2 aspect-video overflow-hidden border'>
-				<Map />
+				<DynamicMap />
 			</div>
 		</div>
 	)
