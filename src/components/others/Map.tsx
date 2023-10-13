4eb3a9d5d2360844988d326548f3180c6d39cba6
FNYukii <forestgreen14689@gmail.com>
2023-10-13 10:22:10 +0900

    feat: leaflet.cssの読み込み&サイズの指定で問題なく表示された

--- src/components/others/Map.tsx
+++ src/components/others/Map.tsx
@@ -1,11 +1,17 @@
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import React from 'react'
+import "leaflet/dist/leaflet.css"
 
 function Map() {
 
 	return (
 
-		<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+		<MapContainer
+			center={[34.7042545, 135.4921653]}
+			zoom={13}
+			scrollWheelZoom={false}
+			style={{ height: "100%", width: "100%" }}
+		>
 
 			<TileLayer
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -13,7 +19,7 @@ function Map() {
 			/>
 
 			<Marker position={[51.505, -0.09]}>
-				
+
 				<Popup>
 					A pretty CSS3 popup. <br /> Easily customizable.
 				</Popup>
