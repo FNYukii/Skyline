ec8308d0d9edcdbd7e1d35330abe944000fc9516
FNYukii <forestgreen14689@gmail.com>
2023-10-13 11:12:54 +0900

    clean: 不要なインポート文を削除

--- src/components/others/Map.tsx
+++ src/components/others/Map.tsx
@@ -2,16 +2,16 @@ import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import "leaflet/dist/leaflet.css"
 
 // マーカー表示を修正
-import L, { LatLngTuple } from "leaflet";
-import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
-import markerIcon from "leaflet/dist/images/marker-icon.png";
-import markerShadow from "leaflet/dist/images/marker-shadow.png";
-delete L.Icon.Default.prototype._getIconUrl;
+import L, { LatLngTuple } from "leaflet"
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png"
+import markerIcon from "leaflet/dist/images/marker-icon.png"
+import markerShadow from "leaflet/dist/images/marker-shadow.png"
+delete L.Icon.Default.prototype._getIconUrl
 L.Icon.Default.mergeOptions({
   iconUrl: markerIcon.src,
   iconRetinaUrl: markerIcon2x.src,
   shadowUrl: markerShadow.src,
-});
+})
 
 interface Props {
 	location: number[]
