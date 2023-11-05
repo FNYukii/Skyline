a38145056e81888a25a3598a3f42bd8bf1572d5a
FNYukii <forestgreen14689@gmail.com>
2023-11-05 22:29:08 +0900

    feat: 読み込みボタンのスタイル設定

--- src/components/sections/PostListSection.tsx
+++ src/components/sections/PostListSection.tsx
@@ -3,6 +3,7 @@ import Image from "next/image"
 import Link from "next/link"
 import { useState } from "react"
 import { MdOutlineImageNotSupported } from "react-icons/md"
+import ReactLoading from "react-loading"
 
 interface Props {
 	posts: Post[]
@@ -12,8 +13,11 @@ interface Props {
 function PostListSection(props: Props) {
 
 	const [posts, setPosts] = useState<Post[]>(props.posts)
+	const [isLoading, setIsLoading] = useState(false)
 
 	async function loadAll() {
+
+		setIsLoading(true)
 
 		try {
 
@@ -24,8 +28,10 @@ function PostListSection(props: Props) {
 			setPosts(posts)
 
 		} catch (error) {
-			alert(`Error!`)
+			alert("Error! API読み出しに失敗しました。")
 		}
+
+		setIsLoading(false)
 	}
 
 	return (
@@ -79,7 +85,20 @@ function PostListSection(props: Props) {
 
 			<div className="flex justify-center">
 
-				<button onClick={() => loadAll()} className="mt-16 py-2 px-24 border border-gray-300 hover:bg-gray-200 transition">すべて見る</button>
+				<button onClick={() => loadAll()} disabled={isLoading} className="mt-16 h-12 w-80 flex justify-center items-center border border-gray-300 hover:bg-gray-200 transition">
+					{!isLoading &&
+						<p>すべて見る</p>
+					}
+
+					{isLoading &&
+						<ReactLoading
+							type="spin"
+							color="#666"
+							height="1.2rem"
+							width="1.2rem"
+						/>
+					}
+				</button>
 			</div>
 		</div>
 	)
