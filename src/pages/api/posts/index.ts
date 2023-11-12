c6ac13e8c1ba105ffc0206c9b4484f36e2105785
FNYukii <forestgreen14689@gmail.com>
2023-11-12 17:33:41 +0900

    feat: API Routes内でallPostsメソッド呼び出しをやめた

--- src/pages/api/posts/index.ts
+++ src/pages/api/posts/index.ts
@@ -2,15 +2,33 @@ import Post from '@/entities/Post'
 import PostService from '@/utilities/PostService'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+import dayjs from "dayjs";
+import fs from 'fs'
+
 type ResponseData = {
 	allPosts: Post[]
 }
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
 
-	return new Promise(async () => {
+	// postsフォルダ内のファイルのファイル名をすべて取得
+	const fileNames = await fs.readdirSync('posts')
 
-		const posts = await PostService.allPosts()
-		return res.status(200).json({ allPosts: posts })
+	// posts配列を生成
+	let posts: Post[] = []
+	fileNames.forEach(fileName => {
+
+		// ベース名(ファイル名から拡張子を除去したもの)
+		const baseName = fileName.replace(/\.md$/, '')
+
+		// ベース名を元にPostを生成
+		const post = PostService.postFromBaseName(baseName)
+		posts.push(post)
 	})
+
+	// postsを新しい順に並べ替え
+	posts.sort((a, b) => dayjs(b.createdAt).toDate().valueOf() - dayjs(a.createdAt).toDate().valueOf())
+
+	// 返す
+	res.status(200).json({ allPosts: posts })
 }
