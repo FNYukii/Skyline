de860c022c88bced48d1661157d5cb31c78bbeb0
FNYukii <forestgreen14689@gmail.com>
2023-11-12 17:36:22 +0900

    feat: API Routes内で外部ファイルのメソッドを一つも呼ばないようにした

--- src/pages/api/posts/index.ts
+++ src/pages/api/posts/index.ts
@@ -2,17 +2,18 @@ import Post from '@/entities/Post'
 import PostService from '@/utilities/PostService'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-import dayjs from "dayjs";
+import dayjs from "dayjs"
 import fs from 'fs'
+import matter from 'gray-matter'
 
 type ResponseData = {
 	allPosts: Post[]
 }
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
+export default function handler(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
 
 	// postsフォルダ内のファイルのファイル名をすべて取得
-	const fileNames = await fs.readdirSync('posts')
+	const fileNames = fs.readdirSync('posts')
 
 	// posts配列を生成
 	let posts: Post[] = []
@@ -22,7 +23,31 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 		const baseName = fileName.replace(/\.md$/, '')
 
 		// ベース名を元にPostを生成
-		const post = PostService.postFromBaseName(baseName)
+
+		// ファイル内のテキスト
+		const textInFile = fs.readFileSync(`posts/${baseName}.md`, 'utf-8')
+
+		// ファイル内のテキストをdataとContentに分離
+		const { data, content } = matter(textInFile)
+
+		// dataオフジェクトからtitle, tags, date, thumbnailプロパティの値を取り出す
+		const title: string = data.title
+		const tags: string[] = data.tags ?? []
+		const createdAt: string = data.createdAt
+		const thumbnail: string | null = data.thumbnail ?? null
+		const location: number[] | null = data.location ?? null
+
+		// ファイルのベース名をidとして、postオブジェクトを生成
+		const post: Post = {
+			id: baseName,
+			title: title,
+			tags: tags,
+			createdAt: createdAt,
+			thumbnail: thumbnail,
+			location: location,
+			content: content
+		}
+		
 		posts.push(post)
 	})
 
