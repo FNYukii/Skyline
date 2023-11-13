4008b38d11a2a83f70e1b969e3b332f0e27e66d5
FNYukii <forestgreen14689@gmail.com>
2023-11-13 20:26:06 +0900

    clean: 不要なファイルを削除

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/app/**/*.{js,ts,jsx,tsx}',
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/app/**/*.{jsx,tsx}',
+    './src/components/**/*.{jsx,tsx}',
     './posts/**/*.md',
   ],
   theme: {
