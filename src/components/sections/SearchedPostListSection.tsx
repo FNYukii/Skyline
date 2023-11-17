20e049cae3cd5904450628f9bc835a16778186fa
FNYukii <forestgreen14689@gmail.com>
2023-11-18 03:23:59 +0900

    clean: tag検索に関するコードを先に書いた

--- src/components/sections/SearchedPostListSection.tsx
+++ src/components/sections/SearchedPostListSection.tsx
@@ -12,40 +12,36 @@ function SearchedPostListSection(props: Props) {
 
 	// クエリパラメータを取得
 	const searchParams = useSearchParams()
-	const keyword = searchParams.get("keyword")
 	const tag = searchParams.get("tag")
+	const keyword = searchParams.get("keyword")
 
 	// keywordとtagどちらもセットされていないなら、404と扱う
-	if (!keyword && !tag) {
-		notFound()
-	}
+	if (!tag && !keyword) notFound()
 
 	// キーワードとタグのうちセットされた方を検索ワードとする
-	const searchWord = keyword ? keyword : tag!
+	const searchWord = tag ? tag : keyword!
 
 	// タグ検索するかどうか
-	const searchType = keyword ? "keyword" : "tag"
+	const searchType = tag ? "tag" : "keyword"
 
 	// searchWordが空なら404へ遷移
-	if (searchWord === "") {
-		notFound()
-	}
+	if (searchWord === "") notFound()
 
 	return (
 
 		<div className={props.className}>
 
 			<div>
-				{searchType === "keyword" &&
-					<h1 className='text-2xl font-bold'>「{searchWord}」の記事</h1>
-				}
-
 				{searchType === "tag" &&
 					<h1 className='text-2xl font-bold'>「{searchWord}」タグの付いた記事</h1>
 				}
+
+				{searchType === "keyword" &&
+					<h1 className='text-2xl font-bold'>「{searchWord}」の記事</h1>
+				}
 			</div>
 
-			<PostListSection posts={[]} className='mt-2'/>
+			<PostListSection posts={[]} className='mt-2' />
 		</div>
 	)
 }
