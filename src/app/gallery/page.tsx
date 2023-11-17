7980cc8c18e7b67e8342b122a85d3c9516e99e94
FNYukii <forestgreen14689@gmail.com>
2023-11-17 10:59:51 +0900

    feat: MapページとGalleryページのファイルを作成

---
+++ src/app/gallery/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Gallery - Skyline'
+}
+
+export default function Page() {
+
+	return (
+
+		<div>
+			<p className='mt-8'>Coming soon.</p>
+		</div>
+	)
+}
---
+++ src/app/map/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Map - Skyline'
+}
+
+export default function Page() {
+
+	return (
+
+		<div>
+			<p className='mt-8'>Coming soon.</p>
+		</div>
+	)
+}
