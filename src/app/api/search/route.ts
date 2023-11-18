2fed07343f7ee02197e074ffc0e95d25bfe604c1
FNYukii <forestgreen14689@gmail.com>
2023-11-18 10:08:31 +0900

    feat: 全てのpostsを返してみた

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,7 +1,10 @@
+import PostService from "@/utils/PostService"
 import { NextRequest, NextResponse } from "next/server"
 
 export function GET(req: NextRequest) {
 
-	const res = NextResponse.json({ message: "Hello" })
+	const posts = PostService.allPosts()
+
+	const res = NextResponse.json({ posts: posts  })
 	return res
 }
