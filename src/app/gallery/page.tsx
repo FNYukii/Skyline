b666b0b9b7cc0bef460e8ae4f67466b99ea1e42c
FNYukii <forestgreen14689@gmail.com>
2023-11-22 16:03:15 +0900

    feat: 本文内の画像のsrcとaltを抽出できた

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -24,11 +24,6 @@ export default function Page() {
 						<div className="relative aspect-video bg-gray-200   hover:brightness-90 hover:cursor-pointer transition">
 							<Image src={photo.src} alt={photo.alt} className="object-cover" fill priority />
 						</div>
-
-						<div className='text-gray-500'>
-							<p>{photo.src}</p>
-							<p>{photo.alt}</p>
-						</div>
 					</div>
 				))}
 			</div>
--- src/utils/PhotoService.ts
+++ src/utils/PhotoService.ts
@@ -1,5 +1,6 @@
 import Photo from "@/entities/Photo"
 import PostService from "./PostService"
+import { randomUUID } from "crypto"
 
 class PhotoService {
 
@@ -7,11 +8,12 @@ class PhotoService {
 
 		const posts = PostService.allPosts()
 
+		// photo要素をまとめる配列photos
 		let photos: Photo[] = []
 
 		posts.forEach(post => {
 
-			// thumnailがあれば配列photosに追加
+			// thumnailがあればphoto要素を追加
 			if (post.thumbnail) {
 
 				photos.push({
@@ -21,34 +23,41 @@ class PhotoService {
 				})
 			}
 
-			// 本文内の画像も配列photosに追加
+			// 本文内の画像も配列photosの要素として追加
 			// 本文文字列内からimgタグのみ抽出
 			const imgs: string[] = post.content.match(/<img.*\/>/g) ?? []
 
-
 			imgs.forEach(img => {
 
-				// imgタグの文字列からsrc属性の値のみ抽出
-				const src: string | null = img.match(/\/images\/.*\.[a-z]*/g)?.at(0) ?? null
+				// imgタグの文字列からsrc="/images/xxxxx.xxx"の部分だけ抽出
+				const srcEqualValue: string | null = img.match(/src="\/images\/.*\.[a-z]*"/)?.at(0) ?? null
+				if (!srcEqualValue) return
+
+				// src="/images/xxxxx.xxx"からsrc属性の値のみ抽出
+				const src: string | null = srcEqualValue.match(/\/images\/.*\.[a-z]*/)?.at(0) ?? null
 				if (!src) return
 
-				// imgタグの文字列からalt属性の値のみ抽出
-				const alt: string | null = "Hello"
+				// imgタグの文字列からalt="xxxxxxxxxxx"の値のみ抽出
+				const altEqualValue: string | null = img.match(/alt=".*"/)?.at(0) ?? null
+				if (!altEqualValue) return
+
+				// alt="xxxxxxxx"からalt属性の値のみ抽出
+				const alt: string | null = altEqualValue.match(/".*"/)?.at(0)?.replace(/"/g, "") ?? null
 				if (!alt) return
 
+				// 新しいphoto要素を追加
 				photos.push({
 					src: src,
 					alt: alt,
 					postId: post.id
 				})
-
 			})
-
-
-
 		})
 
-		return photos
+		// 重複した要素を除外
+		const uniquePhotos = photos.filter((photo, index, photos) => photos.findIndex((e) => e.src === photo.src) === index)
+
+		return uniquePhotos
 	}
 }
 
